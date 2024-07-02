d4b23af98d9ecb8d61f74cc8c4025f63c03d172f
Gallinator <lcgallinator4@gmail.com>
2024-07-02 13:04:50 +0200

    AirfoilDataset: add angle of attack

--- airfoil_dataset.py
+++ airfoil_dataset.py
@@ -6,6 +6,7 @@ from torch.utils.data import Dataset
 class AirfoilDataset(Dataset):
     def __init__(self, data_path: str):
         self.file = h5py.File(data_path, 'r')
+        self.alphas = self.file['alpha']
         self.landmarks = self.file['landmarks']
         self.u = self.file['u']
         self.v = self.file['v']
@@ -13,12 +14,13 @@ class AirfoilDataset(Dataset):
         self.grid_x, self.grid_y = self.file['grid'][()]
 
     def __getitem__(self, item):
+        alpha = torch.tensor([self.alphas[item]], dtype=torch.float32)
         landmark = torch.tensor(self.landmarks[item], dtype=torch.float32)
         u = torch.tensor(self.u[item], dtype=torch.float32)
         v = torch.tensor(self.v[item], dtype=torch.float32)
         p = torch.tensor(self.p[item], dtype=torch.float32)
 
-        return landmark, u, v, p
+        return alpha, landmark, u, v, p
 
     def __len__(self):
         return len(self.landmarks)
