8543559d644996f7e085d6eb3f5e7e572c5be251
Gallinator <lcgallinator4@gmail.com>
2024-07-02 13:22:40 +0200

    Model: pass alpha to forward

--- train.py
+++ train.py
@@ -50,6 +50,7 @@ def train_model(save_path: str):
 
             alpha, landmarks, u, v, p = batch
             landmarks = landmarks.flatten(start_dim=1).to(device)
+            alpha = alpha.to(device)
             u = u.to(device)
             v = v.to(device)
             p = p.to(device)
@@ -58,7 +59,7 @@ def train_model(save_path: str):
             grid_x = torch.tensor(train_data.grid_x, dtype=torch.float32).to(device).repeat(batch_size, 1)
             grid_y = torch.tensor(train_data.grid_y, dtype=torch.float32).to(device).repeat(batch_size, 1)
 
-            pred_u, pred_v, pred_p, _, _, _ = model.forward(grid_x, grid_y, landmarks)
+            pred_u, pred_v, pred_p, _, _, _ = model.forward(alpha, grid_x, grid_y, landmarks)
 
             batch_loss = loss(u, pred_u) + loss(v, pred_v) + loss(p, pred_p)
             batch_loss.backward()
@@ -93,6 +94,7 @@ def evaluate_model(model_path: str):
     for batch in test_loader:
         alpha, landmarks, u, v, p = batch
         landmarks = landmarks.flatten(start_dim=1).to(device)
+        alpha = alpha.to(device)
         u = u.to(device)
         v = v.to(device)
         p = p.to(device)
@@ -101,7 +103,7 @@ def evaluate_model(model_path: str):
         grid_x = torch.tensor(test_data.grid_x, dtype=torch.float32).to(device).repeat(batch_size, 1)
         grid_y = torch.tensor(test_data.grid_y, dtype=torch.float32).to(device).repeat(batch_size, 1)
 
-        pred_u, pred_v, pred_p, _, _, _ = model.forward(grid_x, grid_y, landmarks)
+        pred_u, pred_v, pred_p, _, _, _ = model.forward(alpha, grid_x, grid_y, landmarks)
 
         losses.append(loss(u, pred_u).item())
         losses.append(loss(v, pred_v).item())
