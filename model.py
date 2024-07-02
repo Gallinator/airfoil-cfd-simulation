e98b8e5257e8391a789520d225b4b898d15ebf7b
Gallinator <lcgallinator4@gmail.com>
2024-07-02 20:17:58 +0200

    Model: predict velocities pressure

--- model.py
+++ model.py
@@ -13,14 +13,17 @@ class Model(nn.Module):
             nn.Tanh(),
             nn.Linear(4096, 8192),
             nn.Tanh())
-        self.momentum_x = nn.Sequential(
+        self.v = nn.Sequential(
             nn.Linear(8192, grid_size),
             nn.Tanh())
-        self.momentum_y = nn.Sequential(
+        self.u = nn.Sequential(
+            nn.Linear(8192, grid_size),
+            nn.Tanh())
+        self.p = nn.Sequential(
             nn.Linear(8192, grid_size),
             nn.Tanh())
 
     def forward(self, landmarks, grid_x, grid_y, ) -> tuple:
         in_data = torch.cat((landmarks, grid_x, grid_y), 1)
         x = self.seq(in_data)
-        return self.momentum_x(x), self.momentum_y(x)
+        return self.v(x), self.u(x), self.p(x)
