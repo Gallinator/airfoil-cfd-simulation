7d3a4e697315835704d5d4081cd63ad13c770c7f
Gallinator <lcgallinator4@gmail.com>
2024-07-02 13:04:23 +0200

    AirfoilDataset: use pressure and velocity instead of momentum and density

--- airfoil_dataset.py
+++ airfoil_dataset.py
@@ -7,16 +7,18 @@ class AirfoilDataset(Dataset):
     def __init__(self, data_path: str):
         self.file = h5py.File(data_path, 'r')
         self.landmarks = self.file['landmarks']
-        self.momentum_x = self.file['rho_u']
-        self.momentum_y = self.file['rho_v']
+        self.u = self.file['u']
+        self.v = self.file['v']
+        self.p = self.file['p']
         self.grid_x, self.grid_y = self.file['grid'][()]
 
     def __getitem__(self, item):
         landmark = torch.tensor(self.landmarks[item], dtype=torch.float32)
-        momentum_x = torch.tensor(self.momentum_x[item], dtype=torch.float32)
-        momentum_y = torch.tensor(self.momentum_y[item], dtype=torch.float32)
+        u = torch.tensor(self.u[item], dtype=torch.float32)
+        v = torch.tensor(self.v[item], dtype=torch.float32)
+        p = torch.tensor(self.p[item], dtype=torch.float32)
 
-        return landmark, momentum_x, momentum_y
+        return landmark, u, v, p
 
     def __len__(self):
         return len(self.landmarks)
