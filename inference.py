8a0df1407bd7483978c63df8de9d0fa0d781adca
Gallinator <lcgallinator4@gmail.com>
2024-07-05 20:36:05 +0200

    inference: add alpha slider

--- inference.py
+++ inference.py
@@ -31,6 +31,7 @@ DEFAULT_BEZIER_NODES = [[0.0, 0.0],
                         [0.75, -0.05],
                         [0.25, -0.1],
                         [0.0, -0.1]]
+DEFAULT_ALPHA = 4
 
 
 def sample_beziers(interactor: AirfoilInteractor) -> np.ndarray:
@@ -40,15 +41,34 @@ def sample_beziers(interactor: AirfoilInteractor) -> np.ndarray:
     return np.concatenate((lower_points.T, upper_points.T))
 
 
-def wait_custom_airfoil() -> np.ndarray:
+def deg_to_slope(deg):
+    return math.tan(math.radians(deg))
+
+
+def slope_to_deg(slope):
+    return math.degrees(math.atan(slope))
+
+
+def edit_custom_airfoil() -> tuple:
     upper_polygon = Polygon(np.array(DEFAULT_BEZIER_NODES), animated=True)
-    fig, ax = plt.subplots()
-    ax.add_patch(upper_polygon)
-    airfoil_interactor = AirfoilInteractor(ax, upper_polygon)
-    ax.set_ylim((-0.5, 0.5))
-    ax.set_xlim((-0.5, 1.5))
+    fig, axs = plt.subplots(2, 1, layout='constrained', height_ratios=[0.95, 0.05])
+    airfoil_ax, alpha_ax = axs
+    alpha_line = airfoil_ax.axline((1.0, 0.0), slope=deg_to_slope(DEFAULT_ALPHA), color='blue')
+    airfoil_ax.add_patch(upper_polygon)
+    airfoil_interactor = AirfoilInteractor(airfoil_ax, upper_polygon)
+    airfoil_ax.set_ylim((-0.5, 0.5))
+    airfoil_ax.set_xlim((-0.5, 1.5))
+
+    alpha_slider = Slider(ax=alpha_ax, label='Angle of attack', valmin=-45, valmax=45, valstep=1, valinit=DEFAULT_ALPHA)
+
+    def update_alpha_line(alpha_deg):
+        alpha_line.set_slope(deg_to_slope(alpha_deg))
+        fig.canvas.draw_idle()
+
+    alpha_slider.on_changed(update_alpha_line)
+
     plt.show()
-    return sample_beziers(airfoil_interactor)
+    return sample_beziers(airfoil_interactor), slope_to_deg(alpha_line.get_slope())
 
 
 def main():
@@ -56,8 +76,8 @@ def main():
     features_scaler = load_scaler('data/features_scaler.pkl')
     alpha_scaler = load_scaler('data/alpha_scaler.pkl')
 
-    landmark = wait_custom_airfoil()
-    landmark = normalize_landmarks(landmark, grid_scaler)
+    landmark, alpha = edit_custom_airfoil()
+
     # Needed only to load the grids
     data = AirfoilDataset('data/test_airfoils.h5')
     grid_x = torch.tensor(data.grid_x, dtype=torch.float32).to(device)
