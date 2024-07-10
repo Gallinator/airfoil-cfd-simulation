cb42b5153ba9a2f1c02f4afbc9720fc3310bff64
Gallinator <lcgallinator4@gmail.com>
2024-07-10 14:47:59 +0200

    Model: add cnn encoder and decoder blocks

--- model.py
+++ model.py
@@ -2,6 +2,46 @@ import torch.optim
 from torch import nn
 
 
+class EncoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, batch_norm=True, dropout=0.005,
+                 *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        self.batch_norm = nn.BatchNorm2d(out_channels)
+        self.dropout = nn.Dropout2d(dropout)
+        self.use_batch_norm = batch_norm
+        self.f = nn.LeakyReLU()
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.use_batch_norm:
+            x = self.batch_norm(x)
+        x = self.dropout(x)
+        return self.f(x)
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, batch_norm=True, dropout=0.005,
+                 *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.upsample = nn.Upsample(scale_factor=2)
+        self.padding = nn.ReplicationPad2d(padding)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride)
+        self.batch_norm = nn.BatchNorm2d(out_channels)
+        self.dropout = nn.Dropout2d(dropout)
+        self.use_batch_norm = batch_norm
+        self.f = nn.LeakyReLU()
+
+    def forward(self, x):
+        x = self.upsample(x)
+        x = self.padding(x)
+        x = self.conv(x)
+        if self.use_batch_norm:
+            x = self.batch_norm(x)
+        x = self.dropout(x)
+        return self.f(x)
+
+
 class Model(nn.Module):
     def __init__(self, grid_size: int, in_size: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
