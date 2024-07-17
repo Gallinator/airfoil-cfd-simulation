c0e94e7e5495fdb7588105aa6099ce531006ee67
Gallinator <lcgallinator4@gmail.com>
2024-07-17 12:30:33 +0200

    data_preprocessing: allow to use all data from both angle of attacks, improve shuffling without copying

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -98,23 +98,15 @@ def airfoil_sampling_task(i, airfoil_poly, sample_grid, ff):
     return i, rho_u, rho_v, rho, e, 1 * airfoil_mask
 
 
-def get_flow_fields(src: File, indices, alphas) -> list:
+def get_flow_fields(src: File, indices) -> list:
     # As h5py groups do not support slicing the only way to select item is to iterate over the whole group
     ff = [None for _ in range(len(indices))]
-    for i, item in enumerate(src[f'alpha+12']['flow_field'].items()):
+    for i, item in enumerate(src['flow_field'].items()):
         try:
             idx = indices.index(i)
         except ValueError:
             continue
-        if alphas[idx] == '12':
-            ff[idx] = item
-    for i, item in enumerate(src[f'alpha+04']['flow_field'].items()):
-        try:
-            idx = indices.index(i)
-        except ValueError:
-            continue
-        if alphas[idx] == '04':
-            ff[idx] = item
+        ff[idx] = item
     return ff
 
 
@@ -179,19 +171,24 @@ def load_scaler(path: str):
     return pickle.load(open(path, 'rb'))
 
 
-def extract_coefs(src: File, indices, alphas) -> tuple:
-    cd = np.array([None for _ in range(len(indices))])
-    cl = np.array([None for _ in range(len(indices))])
-    cm = np.array([None for _ in range(len(indices))])
-    alphas_12 = alphas[indices] == 12
-    alphas_04 = alphas[indices] == 4
-    cd[alphas_12] = src[f'alpha+12']['C_d'][()][indices][alphas_12]
-    cd[alphas_04] = src[f'alpha+04']['C_d'][()][indices][alphas_04]
-    cl[alphas_12] = src[f'alpha+12']['C_l'][()][indices][alphas_12]
-    cl[alphas_04] = src[f'alpha+04']['C_l'][()][indices][alphas_04]
-    cm[alphas_12] = src[f'alpha+12']['C_m'][()][indices][alphas_12]
-    cm[alphas_04] = src[f'alpha+04']['C_m'][()][indices][alphas_04]
+def extract_coefs(src: File, indices) -> tuple:
+    cd = src['C_d'][()][indices]
+    cl = src['C_l'][()][indices]
+    cm = src['C_m'][()][indices]
     return cd, cl, cm
+
+
+def shuffle_data(*data: np.ndarray):
+    seed = np.random.randint(0, 2 ** (32 - 1) - 1)
+    for d in data:
+        rstate = np.random.RandomState(seed)
+        rstate.shuffle(d)
+
+
+def generate_indices(data_len, num_samples):
+    indices = np.arange(data_len, dtype=int)
+    np.random.shuffle(indices)
+    return indices[:num_samples]
 
 
 def create_sampled_datasets(source_path: str, dest_path: str, sample_grid_size, num_samples: int, train_size: float):
@@ -204,20 +201,21 @@ def create_sampled_datasets(source_path: str, dest_path: str, sample_grid_size, 
 
     with (h5pickle.File(source_path, 'r') as source):
         landmarks = source['shape']['landmarks'][()]
-        num_airfoils = len(landmarks)
-
-        indices = np.arange(num_airfoils, dtype=int)
-        np.random.shuffle(indices)
-        indices = indices[:num_samples]
-        landmarks = landmarks[indices]
-        alphas = random.choices(['04', '12'], k=num_airfoils)
+        data_len = len(landmarks)
+        per_alpha_samples = num_samples // 2
+        indices_04 = generate_indices(data_len, per_alpha_samples)
+        indices_12 = generate_indices(data_len, per_alpha_samples)
+        all_indices = np.concatenate([indices_04, indices_12])
+        landmarks = landmarks[all_indices]
+        alphas = ['04'] * per_alpha_samples + ['12'] * per_alpha_samples
         train_end = int(num_samples * train_size)
 
         grid_x, grid_y = np.mgrid[-0.5:1.5:sample_grid_size, -1:1:sample_grid_size]
 
         u, v, rho, energy, masks = [[None] * num_samples for _ in range(5)]
         with multiprocessing.Pool() as pool:
-            flow_fields = get_flow_fields(source, indices.tolist(), alphas)
+            flow_fields = (get_flow_fields(source['alpha+04'], indices_04.tolist()) +
+                           get_flow_fields(source['alpha+12'], indices_12.tolist()))
             args = [(i, landmarks[i], (grid_x, grid_y), ff[1]) for i, ff in enumerate(flow_fields)]
             for i, r_u, r_v, r, e, m in pool.starmap(airfoil_sampling_task, args):
                 u[i] = np.divide(r_u, r, np.zeros_like(r), where=r != 0)
@@ -227,7 +225,11 @@ def create_sampled_datasets(source_path: str, dest_path: str, sample_grid_size, 
                 masks[i] = m
 
         alphas = np.array([int(a) for _, a in enumerate(alphas)])
-        c_d, c_l, c_m = extract_coefs(source, indices, alphas)
+        coefs_04 = extract_coefs(source['alpha+04'], indices_04)
+        coefs_12 = extract_coefs(source['alpha+12'], indices_12)
+        c_d, c_l, c_m = np.concatenate((coefs_04, coefs_12), 1)
+
+        shuffle_data(landmarks, alphas, masks, u, v, rho, energy, c_d, c_l, c_m)
 
         train_cd, train_cl, train_cm, coefs_scaler = normalize_coefficients(
             c_d[:train_end], c_l[:train_end], c_m[:train_end])
