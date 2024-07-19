88361ec8af2fe0fb9f8fc0b5883265d073f701b1
Gallinator <lcgallinator4@gmail.com>
2024-07-19 23:06:49 +0200

    LossTracker: add docstrings

--- loss_tracker.py
+++ loss_tracker.py
@@ -2,11 +2,21 @@ import numpy as np
 
 
 class LossTracker:
+    """
+    Helper class to track training losses across epochs and batches.
+    batch_update() must be called at the end of each batch, epoch_update() must be called at the end of each epoch.
+    loss history contains the batch average losses of each epoch.
+    Supports multiple losses
+    """
+
     def __init__(self, *loss_names):
         self.loss_history = {k: [] for k in loss_names}
         self._batch_loss_history = {k: [] for k in loss_names}
 
     def batch_update(self, **losses):
+        """
+        :param losses: the actual losses. kwargs must use the same names as the ones used in the constructor
+        """
         for k, v in losses.items():
             self._batch_loss_history[k].append(v)
 
